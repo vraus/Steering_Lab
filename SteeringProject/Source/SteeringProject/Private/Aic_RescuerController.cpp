e56a4f154ed33f99189f133060dae51406ff8381
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2025-02-26 12:01:22 +0100

    Base configuration Rescuer
    Refactored this level :
     - BPI to get points world position from spline
     - Base classe for AI Controller
     - Spawner of Rescuer Actor

---
+++ SteeringProject/Source/SteeringProject/Private/Aic_RescuerController.cpp
@@ -0,0 +1,4 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Aic_RescuerController.h"
---
+++ SteeringProject/Source/SteeringProject/Public/Aic_RescuerController.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "Aic_RescuerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class STEERINGPROJECT_API AAic_RescuerController : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	
+};
