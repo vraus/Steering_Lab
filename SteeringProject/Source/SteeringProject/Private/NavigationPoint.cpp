29618904f4d439d01b87e18ac047e4cfa266ebd7
Hadrien Belleville <85734187+vraus@users.noreply.github.com>
2025-03-06 13:59:51 +0100

    C++ navigation point actor
    C++ nav point actor base class
    removed struct nav node
    
    TODO: Rescuer agent in C++ and fix A* using C++ functions instead of BPs

---
+++ SteeringProject/Source/SteeringProject/Private/NavigationPoint.cpp
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "NavigationPoint.h"
+
+#include "Kismet/GameplayStatics.h"
+
+
+ANavigationPoint::ANavigationPoint()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+
+	ConnectionRadius = 800.f;
+}
+
+void ANavigationPoint::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FindNearbyNavPoints();
+}
+
+void ANavigationPoint::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	DrawConnections();
+}
+
+void ANavigationPoint::FindNearbyNavPoints()
+{
+	TArray<AActor*> FoundActors;
+	// UGameplayStatics::GetAllActorsOfClass(GetWorld(), TSubclassOf<ANavigationPoint>(), FoundActors);
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ANavigationPoint::StaticClass(), FoundActors);
+
+	for (const auto Actor : FoundActors)
+	{
+		if (this->GetDistanceTo(Actor) <= ConnectionRadius)
+			ConnectedPoints.Add(static_cast<ANavigationPoint*>(Actor));
+	}
+}
+
+void ANavigationPoint::DrawConnections() const
+{
+	for (const auto Connection : ConnectedPoints)
+	{
+		DrawDebugLine(GetWorld(), this->GetActorLocation(), Connection->GetActorLocation(), FColor::Red);
+	}
+}
---
+++ SteeringProject/Source/SteeringProject/Public/NavigationPoint.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "NavigationPoint.generated.h"
+
+UCLASS(Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class STEERINGPROJECT_API ANavigationPoint : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ANavigationPoint();
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+
+protected:
+
+	UFUNCTION(BlueprintCallable, Category = "Navigation")
+	void FindNearbyNavPoints();
+
+private:
+
+	UFUNCTION(BlueprintCallable, Category="Debug")
+	void DrawConnections() const;
+
+public:
+	UPROPERTY(BlueprintReadWrite, VisibleAnywhere, Category = "Components")
+	USceneComponent* Root;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, VisibleAnywhere, Category = "Navigation")
+	TArray<ANavigationPoint*> ConnectedPoints;
+
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly, Category = "Navigation")
+	float ConnectionRadius;
+};
